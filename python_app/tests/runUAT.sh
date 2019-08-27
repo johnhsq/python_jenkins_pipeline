c9e5a13fe2227e2d4a953388c641a138020967e6
John Huang <johnhsq@yahoo.com>
2019-08-26 17:02:26 -0700

    Update runUAT.sh

--- python_app/tests/runUAT.sh
+++ python_app/tests/runUAT.sh
@@ -8,12 +8,13 @@ port=$1
 sleep 20 
 
 # ping the app
-status_code=$(curl --write-out %{http_code} --out /dev/null --silent $hostname:$port)
+status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
 
 if [ $status_code == 200 ];
 then
 	echo "PASS: ${hostname}:${port} is reachable"
 else
+	echo $status_code
 	echo "FAIL: ${hostname}:${port} is unreachable"
     exit 1
 fi
